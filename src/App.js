b4e9b3729be766ef9013213e9c934a7e9f62a2d5
Marylene3925 <58249807+Marylene3925@users.noreply.github.com>
2020-04-21 11:42:56 +0200

    affichages des films

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component } from "react";
+import "./App.css";
+import ListFilms from "./components/Films"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Bienvenu sur le premier site react
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="films">
+//       <ListFilms></ListFilms>
+//     </div>
+//   );
+// }
+
+class App extends Component{
+  render (){
+    return (
+      <div className="films">
+      <ListFilms></ListFilms>      
     </div>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Films.js
@@ -0,0 +1,67 @@
+import React, {Components} from 'react';
+
+//le composant : il y à un constructeur qui va initiatiliser 3 "state: erreur, isLoaded, items".c'est un tableau vide pour linstant
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isLoaded: false,
+        items: []
+      };
+    }
+  
+   
+    componentDidMount() {
+     // fecth permet d'appeler notre api avec l'url
+      fetch("https://api.themoviedb.org/4/list/1?page=1&api_key=12d9c0e3fda01905082fcc893d329dfc")
+      .then(results => results.json())
+        //quand l'url aura bien été appellée, isloaded passera à true et nos items vont être "result.items"
+        .then(json => {
+          this.setState({
+              isLoaded : true,
+              items: json, 
+          })
+       });
+      }
+        
+    
+    render() {
+      const { error, isLoaded, items } = this.state;
+      const chemin = 'https://image.tmdb.org/t/p/w500'      
+ 
+      //sinon on lit chargement
+      if (!isLoaded) {
+        return <div>Chargement…</div>;
+      }
+      //Sinon on a un retourn d'une liste de ul et chacun de nos items est dans un "li"
+       else {
+        return (
+          <div className="App table-films">
+            <table>
+              <tr>
+                          
+          {/* { console.log(items.results) } */}
+              
+              {items.results.map(item => (
+                  <td>
+                    <li key={item.id}>
+                  
+                  <img src={chemin + item.poster_path} alt={item.title} />
+                  echo=""
+                  {item.title}
+                  
+                  {item.vote_average} 
+                      
+                </li>
+               </td> 
+              ))}
+          
+              </tr>
+            </table>
+          </div>
+        );
+      }
+    }
+  }
+
+  export default App;
